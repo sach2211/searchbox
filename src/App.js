4fe2db2b7b98cc372089f3c36ec920a60a0f8bfc
sach11 <sachin.chopra@myntra.com>
2017-08-02 15:19:20 +0530

    ui refactoring. working state.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,89 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import request from 'superagent';
 import './App.css';
+import throttle from 'lodash.throttle';
+
+var flag = null;
+var searchSuggestionLimit = 5;
+var savedSearches = [{product: "your"}, {product: "saved"}, {product: "searches"}];
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      q: '',
+      searchResults: savedSearches
+    }
+  }
+
+  handleInputTerm = (q) => {
+    this.setState((prevState, props) => (
+      {q: q}
+    ));
+    
+    // handle the case where search term is null (show top searches ???)
+    if (q) {
+      this.getSearchResults(q);
+    } else {
+      flag && flag.abort();
+      this.setState({ searchResults: savedSearches });
+    }
+  }
+
+  getSearchResults(q) {
+    if (flag) {
+      console.log(" Flag was set aborting it ");
+      flag.abort();
+    }
+    flag = request('https://crossorigin.me/http://developer.myntra.com/search/data/'+q)
+    .end((err, resp) => {
+      if (err) {
+        console.error("An error in response for term", q);
+        return;
+      }
+      flag = null;
+      let x = resp.body.data.results.products || [];
+      this.setState({searchResults: x.splice(0, searchSuggestionLimit)});
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="search-box-cont">
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-input"
+            onChange={(e) => {
+                this.handleInputTerm(e.target.value)
+              }
+            }
+          />
+          {
+            this.state.searchResults
+            ?
+              <div className="search-results-cont"> 
+                {
+                  this.state.searchResults.map((searchRes, i) => {
+                    return (
+                      <div className="search-results">
+                        {
+                          searchRes.product.length > 20 
+                          ?  searchRes.product.substr(0, 17) + '...'
+                          :  searchRes.product
+                        }
+                      </div>
+                    )
+                  }, this)
+                }
+              </div>
+            :
+              null
+          }
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="content"> 
+           Some random content on the page 
+        </div>
       </div>
     );
   }
